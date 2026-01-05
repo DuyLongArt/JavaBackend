b5444124b20b7f7f3d8fa32fe530e68e9059df2a
DuyLong <duylongmind432001@gmail.com>
2026-01-05 21:13:48 +0700

    Add allow all origin

--- src/main/java/backend/WebConfig.java
+++ src/main/java/backend/WebConfig.java
@@ -1,6 +1,5 @@
 package backend;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,24 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 // Place this in a file like 'WebConfig.java' in your configuration package.
 
 @Configuration
-class WebConfig
-{
+class WebConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer()
-        {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry)
-            {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Applies CORS configuration to all endpoints
-                        .allowedOrigins(
-                                "http://localhost:*",
-                                "http://localhost:22222",
-                                "http://192.168.1.4:22222",
-                                "http://169.254.253.176:22222"
-                        ) // Allows requests from your Vite dev server origins
+                        .allowedOrigins("*") // Allows requests from any origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allows specified HTTP methods
                         .allowedHeaders("*") // Allows all headers;
                         .allowCredentials(true); // Allows cookies and credentials
@@ -36,4 +26,3 @@ class WebConfig
         };
     }
 }
-
