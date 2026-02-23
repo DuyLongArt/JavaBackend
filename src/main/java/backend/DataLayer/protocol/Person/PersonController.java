6827ae756e481c8ff3c9a95b8f85505826493fff
DuyLong <duylongmind432001@gmail.com>
2026-02-23 21:47:07 +0700

    Fix mirror bug

--- src/main/java/backend/DataLayer/protocol/Person/PersonController.java
+++ src/main/java/backend/DataLayer/protocol/Person/PersonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/backend/person/")
+@RequestMapping("/backend/person")
 
 public class PersonController {
     @Autowired
